a7f026d9aa043e46a5a83d45d8ec5e27ec3935a9
Ex0dIa-dev <lucafranze3012@gmail.com>
2021-06-02 14:49:48 +0200

    Completed CalculateCodiceFiscale() in CodiceFiscale.java

--- src/CodiceFiscale.java
+++ src/CodiceFiscale.java
@@ -1,2 +1,17 @@
+import static helper.CFMethods.*;
+
 public class CodiceFiscale {
+
+    public static String CalculateCodiceFiscale(String surname, String name, String birthday,String gender, String city) {
+
+        String cf = "";
+        cf += GetSurname(surname);
+        cf += GetName(name);
+        cf += GetBirthdayGender(birthday, gender);
+        cf += GetCityCode(city);
+        cf += GetControlCode(cf);
+
+        return cf;
+    }
+
 }
