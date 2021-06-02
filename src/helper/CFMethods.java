d36958b4d1d5f835ef206f0a549ea346b6d3ab00
Ex0dIa-dev <lucafranze3012@gmail.com>
2021-06-02 14:31:55 +0200

    completed GetControlCode() method in CFMethods.java, and added controlcode dir in data/

--- src/helper/CFMethods.java
+++ src/helper/CFMethods.java
@@ -9,6 +9,7 @@ import java.io.FileReader;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
 // Contains all methods for generating Codice Fiscale(Italiano)
 public class CFMethods {
@@ -103,6 +104,50 @@ public class CFMethods {
         } else {
             return "error";
         }
+    }
+
+    //take in input an incomplete Codice Fiscale, return the final Control Code
+    public static String GetControlCode(String cf) {
+
+        //reading and parsing Json file for calculating Control Code
+        JSONParser parser = new JSONParser();
+        JSONObject control_code = new JSONObject();
+        JSONObject even_char_value = new JSONObject();
+        JSONObject odd_char_value = new JSONObject();
+
+        try {
+            Object obj_cc = parser.parse(new FileReader("./data/controlcode/controlcode.json"));
+            Object obj_ech = parser.parse(new FileReader("./data/controlcode/even_char_value.json"));
+            Object obj_och = parser.parse(new FileReader("./data/controlcode/odd_char_value.json"));
+
+            control_code = (JSONObject) obj_cc;
+            even_char_value = (JSONObject) obj_ech;
+            odd_char_value = (JSONObject) obj_och;
+        } catch (Exception e) {e.printStackTrace();}
+
+        //checking if Codice Fiscale length is 15
+        if (cf.length() != 15) {return "cf-length-error";}
+
+        int n = 0;
+        ArrayList<String> even_ch = new ArrayList<>();
+        ArrayList<String> odd_ch = new ArrayList<>();
+
+        //extracting chars in even or odd position
+        for (int ch=0;ch<cf.length();ch++) {
+            if (n%2==0) {even_ch.add(Character.toString(cf.charAt(ch)));} else {odd_ch.add(Character.toString(cf.charAt(ch)));}
+            n++;
+        }
+
+        //converting to Integer the chars
+        int tot = 0;
+        for (String ch : odd_ch) {
+            if("0123456789".contains(ch)) {tot += Integer.parseInt(ch);} else {tot += Integer.parseInt(String.valueOf(odd_char_value.get(ch)));}
+        }
+        for (String ch: even_ch) {
+            tot += Integer.parseInt(String.valueOf(even_char_value.get(ch)));
+        }
+
+        return (String) control_code.get(String.valueOf(tot%26));
     }
 
 }
